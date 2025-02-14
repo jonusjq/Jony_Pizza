c545a45499a0892cecbf47df19a41a1a9c5e12e5
source_dev <prifaryt@gmail.com>
2025-02-14 14:45:10 +0300

    seventh commit

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -3,9 +3,7 @@ import { Container } from "./container";
 import * as React from "react";
 import Image from "next/image";
 import { Button } from "../ui";
-import { User } from "lucide-react";
-import { ShoppingCart } from "lucide-react";
-import { Search } from "lucide-react";
+import { Search, ShoppingCart, ArrowRight, User } from "lucide-react";
 
 
 interface Props {
@@ -15,22 +13,32 @@ export const Header: React.FC<Props> = ({ className }) => {
   return (
     <header className={cn("border border-b", className)}>
       <Container className="flex items-center justify-between py-8">
-        {/* Правая часть */}
+        {/* левая часть */}
         <div className="flex items-center gap-4">
           <Image src="/logo.png" alt="лого" width={35} height={35} />
           <div>
-            <h1 className=" font-black">next pizza</h1>
-            <p>вкуснеее уже некуда</p>
+            <h1 className="text-2xl uppercase font-black">next pizza</h1>
+            <p className="text-sm text-gray-400 leading-3">вкуснеее уже некуда</p>
           </div>
         </div>
-        {/* левая часть */}
-        <div className="flex width-25">
-            <Button variant="outline" className="mr-4">
-            <User width={20} height={20} />профиль
+        {/* правая часть */}
+        <div className="flex items-center gap-3">
+            <Button variant="outline" className="flex items-center gap-3">
+            <User size={16}/>Войти
             </Button>
             <Button variant="outline">
-            <ShoppingCart />
+            <ShoppingCart size={16}/>
             </Button>
+            <div>
+              <Button className="group relative">
+                <b>520 ₽</b>
+                <span className="h-full w-[1px] bg-white/30 mx-3"></span>
+                <div className="flex items-center gap-1">
+                <ShoppingCart size={16} className="relative" strokeWidth={2} />
+                <b>3</b>
+                </div>
+              </Button>
+            </div>
         </div>
       </Container>
     </header>
